c9b7c1358717f6938679740cdcb245d7dde65e5c
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-25 16:10:03 -0500

    closed capture user input issue

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const printProfileData = profileDataArr => {
     console.log('================');
 
     // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-    });
+    profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+
+printProfileData(profileDataArgs);
