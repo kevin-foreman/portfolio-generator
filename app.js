a291d1fd2ce93ac38e572391a665422072f34c33
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-28 13:22:45 -0500

    incorporated some changes from the code snapshot

--- app.js
+++ app.js
@@ -1,16 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
-
-// const fs = require('fs');
-
-// const generatePage = require('./src/page-template.js');
-
-// const  pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -145,4 +135,10 @@ promptUser()
 .then(promptProject)
 .then(portfolioData => {
     console.log(portfolioData);
+    // will be uncommented in lesson 4
+    // const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
 });
